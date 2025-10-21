04840c204da04b7796527377062b9866abdbcaeb
cynpy <stepan.grebnev.05@mail.ru>
2025-10-21 15:32:44 +0500

    Выполнил задачу 3

--- Game/Domain/MongoGameRepository.cs
+++ Game/Domain/MongoGameRepository.cs
@@ -4,42 +4,51 @@ using MongoDB.Driver;
 
 namespace Game.Domain
 {
-    // TODO Сделать по аналогии с MongoUserRepository
     public class MongoGameRepository : IGameRepository
     {
+        private readonly IMongoCollection<GameEntity> gameCollection;
         public const string CollectionName = "games";
 
         public MongoGameRepository(IMongoDatabase db)
         {
+            gameCollection = db.GetCollection<GameEntity>(CollectionName);
         }
 
         public GameEntity Insert(GameEntity game)
         {
-            throw new NotImplementedException();
+            gameCollection.InsertOne(game);
+            return game;
         }
 
         public GameEntity FindById(Guid gameId)
         {
-            throw new NotImplementedException();
+            return gameCollection.Find(g => g.Id == gameId).FirstOrDefault();
         }
 
         public void Update(GameEntity game)
         {
-            throw new NotImplementedException();
+            gameCollection.ReplaceOne(g => g.Id == game.Id, game);
         }
 
         // Возвращает не более чем limit игр со статусом GameStatus.WaitingToStart
         public IList<GameEntity> FindWaitingToStart(int limit)
         {
-            //TODO: Используй Find и Limit
-            throw new NotImplementedException();
+            return gameCollection
+                .Find(g => g.Status == GameStatus.WaitingToStart)
+                .Limit(limit)
+                .ToList();
         }
 
         // Обновляет игру, если она находится в статусе GameStatus.WaitingToStart
         public bool TryUpdateWaitingToStart(GameEntity game)
         {
-            //TODO: Для проверки успешности используй IsAcknowledged и ModifiedCount из результата
-            throw new NotImplementedException();
+            var filter = Builders<GameEntity>.Filter.And(
+                Builders<GameEntity>.Filter.Eq(g => g.Id, game.Id),
+                Builders<GameEntity>.Filter.Eq(g => g.Status, GameStatus.WaitingToStart)
+            );
+            
+            var result = gameCollection.ReplaceOne(filter, game);
+            return result.IsAcknowledged && result.ModifiedCount > 0;
         }
     }
 }
